11014c7d78ad97aee82d7f196dc8c0aa702490c9
NightcoreBomb <150283698+NightcoreBomb@users.noreply.github.com>
2023-11-08 22:28:04 +0200

    Delete dev-school-2 directory

--- dev-school-2/123ц2312.js
+++
@@ -1,6 +0,0 @@
-
-if (10%5 === 0) {
-    console.log(10 % 5)
-}
-
-console.log(11%5)
--- dev-school-2/health.js
+++
@@ -1,9 +0,0 @@
-
-
-for (let health = 10; health < 100; health++){
-    console.log("Відновлення здоров'я у процесі, поточний рівень:" + health)
-}
-if (health=100){
-    console.log("Вітаю ти здорова")
-}
-//gift for Ria
